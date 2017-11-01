a7ea5e8a5d3b3efa7c7bfba00b24cebe9acab91b
Colleen Brents <cbrents1@rams.colostate.edu>
2017-11-01 11:33:35 -0600

    sample data

--- sampledata.R
+++ sampledata.R
@@ -2,6 +2,7 @@
 library(readxl)
 library(tidyverse)
 library(stringr)
+
 #just file name because already working in group_2 directory 
 testdata <- read_excel("DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", 
                        skip = 1, n_max = 12, sheet = )
