5c71b2f886dbc2407bcda085e5cd3f6297374924
Colleen Brents <cbrents1@rams.colostate.edu>
2017-11-08 12:03:22 -0700

    updated link to sample data

--- sample_data/sampledata.R
+++ sample_data/sampledata.R
@@ -6,5 +6,7 @@ library(stringr)
 library(readr)
 test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
                              "master/CSV_Files/efficacy_summary.csv"))
+
 #testdata <- read_excel("DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", skip = 1, n_max = 12, sheet = )
-summary(testdata)
+summary(test_data)
+head(test_data)
