21374597af444cf27e9630f5a40665fc88b0b0c5
Mackenzie Fry <mfry@colostate.edu>
2017-11-08 11:23:25 -0700

    example of our data with ggraph library

---
+++ Example_ggraph.R
@@ -0,0 +1,17 @@
+library(readr)
+library(dplyr)
+library(ggraph)
+
+test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                             "master/CSV_Files/efficacy_summary.csv"))
+
+test_data_dend <- test_data %>% 
+  select(PLA:SLE, cLogP:MacUptake) %>% 
+  as.matrix() %>% 
+  t() %>% 
+  dist() %>% 
+  hclust() %>% 
+  as.dendrogram()
+
+ggraph(test_data_dend, layout = "dendrogram") + 
+  geom_edge_diagonal()
