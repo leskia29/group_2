2bb772d486061f57da2f762ef147f0ec6cdaa853
Mackenzie Fry <mfry@colostate.edu>
2017-11-08 11:28:52 -0700

    moved to sample_data folder

---
+++ sample_data/sampledata.R
@@ -0,0 +1,10 @@
+#read in xlsx to github
+library(readxl)
+library(tidyverse)
+library(stringr)
+#just file name because already working in group_2 directory 
+library(readr)
+test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                             "master/CSV_Files/efficacy_summary.csv"))
+#testdata <- read_excel("DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", skip = 1, n_max = 12, sheet = )
+summary(testdata)
