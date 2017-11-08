d4938e4070f07aa72dbbc820b0e55e0f5b30c0ef
Colleen Brents <cbrents1@rams.colostate.edu>
2017-11-08 11:22:17 -0700

    revised sampledata puller

--- sampledata.R
+++ sampledata.R
@@ -3,6 +3,8 @@ library(readxl)
 library(tidyverse)
 library(stringr)
 #just file name because already working in group_2 directory 
-testdata <- read_excel("DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", 
-                       skip = 1, n_max = 12, sheet = )
+library(readr)
+test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                             "master/CSV_Files/efficacy_summary.csv"))
+#testdata <- read_excel("DRUG_CLASS_I_Mean_Cmax_Trough_Efficacy_R_DATA_ANALYSIS.xlsx", skip = 1, n_max = 12, sheet = )
 summary(testdata)
