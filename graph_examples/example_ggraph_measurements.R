b5b075d009e0a77f9e0079f5d84a89d4f5cb93a7
Mackenzie Fry <mfry@colostate.edu>
2017-11-13 11:23:29 -0700

    ggraph example with measurements**updated

---
+++ graph_examples/example_ggraph_measurements.R
@@ -0,0 +1,24 @@
+library(readr)
+library(dplyr)
+library(ggraph)
+library(dendextend)
+
+test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                             "master/CSV_Files/efficacy_summary.csv"))
+
+test_data_dend <- test_data %>% 
+  select(PLA:SLE,cLogP:MacUptake) %>% 
+  mutate_all(funs(scale(.))) %>% 
+  as.matrix() %>% 
+  t() %>% 
+  dist() %>% 
+  hclust() %>% 
+  as.dendrogram(horiz = TRUE, hang = .3)
+
+
+plot(test_data_dend,
+     xlab = "",
+     ylab = "",
+     main = "Cluster Example",
+     horiz = TRUE)
+
