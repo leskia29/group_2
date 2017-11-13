84ffea3a5afde9c0125bb229168a1e4bce75474d
Marialexia Alfano <marialexia.alfano@colostate.edu>
2017-11-13 11:18:59 -0700

    playing around with small multiples and organizing code

--- sample_data/ggbeeswarm_sample_data.R
+++ sample_data/ggbeeswarm_sample_data.R
@@ -23,7 +23,7 @@ bio_sample_test <- test_data %>%
 head(bio_sample_test)
 
 
-#single variable plot 
+#single variable plot for bio marker
 test_plot <- bio_sample_test %>% 
   ggplot(aes(dosage, PLA, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
@@ -31,15 +31,17 @@ test_plot <- bio_sample_test %>%
   ggtitle('geom_beeswarm') + labs(x='Dose')+
   theme_few()
 
+test_plot
 
 #gather for small multiples bio markers
-
 bio_sample_forSM <- bio_sample_test %>% 
   gather(key = variable, value = value, -drug, -dosage)
 
+head(bio_sample_forSM)
+
 
 #plot small multiples bio markers 
-bio_sample_forSM %>% 
+bio_sample_SMplot <- bio_sample_forSM %>% 
   ggplot(aes(x = dosage, y = value, color = drug))+
   geom_beeswarm(alpha = 0.5, size = 1.5)+
   scale_x_continuous(breaks = c(50,100))+
@@ -47,13 +49,41 @@ bio_sample_forSM %>%
   theme_few()+
   facet_wrap(~ variable, ncol = 4)
 
+bio_sample_SMplot
 
+#####################################################################
 
+#select for chem marker variables
+chem_sample_test <- test_data %>%
+  select(drug, dosage, cLogP, huPPB, muPPB, 
+         MIC_Erdman, MICserumErd, MIC_Rv, Caseum_binding, MacUptake) 
 
+head(chem_sample_test)
 
+#single variable plot for chem marker
+test_plot_2 <- chem_sample_test %>% 
+  ggplot(aes(dosage, cLogP, color = drug))+
+  geom_beeswarm(alpha=.5,size=1.5)+
+  scale_x_continuous(breaks=c(50,100))+
+  ggtitle('geom_beeswarm') + labs(x='Dose')+
+  theme_few()
 
+test_plot_2
 
+#gather for small multiples chem markers
+chem_sample_forSM <- chem_sample_test %>% 
+  gather(key = variable, value = value, -drug, -dosage)
 
+head(chem_sample_forSM)
 
+#plot small multiples chem markers 
+chem_sample_SMplot <- chem_sample_forSM %>% 
+  ggplot(aes(x = dosage, y = value, color = drug))+
+  geom_beeswarm(alpha = 0.5, size = 1.5)+
+  scale_x_continuous(breaks = c(50,100))+
+  labs(x = 'Dose', y = 'Value')+
+  theme_few()+
+  facet_wrap(~ variable, ncol = 4, scale="free")
 
+chem_sample_SMplot
 
