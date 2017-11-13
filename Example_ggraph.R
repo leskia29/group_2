3a04a5364a54e1ea8dd446c465cb58192d6e6fa4
Mackenzie Fry <mfry@colostate.edu>
2017-11-13 11:21:46 -0700

    ggraph-across measurements

--- Example_ggraph.R
+++ Example_ggraph.R
@@ -1,20 +1,35 @@
 library(readr)
 library(dplyr)
 library(ggraph)
+library(dendextend)
+
+
 
 test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
                              "master/CSV_Files/efficacy_summary.csv"))
 
 test_data_dend <- test_data %>% 
-  select(PLA:SLE) %>% 
+  select(PLA:SLE,cLogP:MacUptake) %>% 
   mutate_all(funs(scale(.))) %>% 
   as.matrix() %>% 
   t() %>% 
   dist() %>% 
   hclust() %>% 
-  as.dendrogram()
+  as.dendrogram(horiz = TRUE, hang = .3)
 
-ggraph(test_data_dend, layout = "dendrogram") + 
-  geom_edge_diagonal()
+test_data_dend<- rotate(dendogram, 1:21)
+  
 
 
+
+(test_data_dend) %>% 
+plot(xlab = "",
+     ylab = "",
+     main = "Cluster Example")
+
+  
+  
+
+
+?plot.hclust
+?dendextend
