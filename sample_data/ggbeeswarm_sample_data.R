c90bc0a6dbf406f29f6af990dc98a88174945b2e
Marialexia Alfano <marialexia.alfano@colostate.edu>
2017-11-13 10:58:51 -0700

    figuring out small multiples for beeswarm

--- sample_data/ggbeeswarm_sample_data.R
+++ sample_data/ggbeeswarm_sample_data.R
@@ -5,6 +5,9 @@ library(readr)
 library(ggplot2)
 library(ggbeeswarm)
 library(ggthemes)
+library(tidyr)
+library(scales)
+
 
 test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
                              "master/CSV_Files/efficacy_summary.csv"))
@@ -13,17 +16,41 @@ summary(test_data)
 head(test_data)
 
 
+#select for bio marker variables
 bio_sample_test <- test_data %>%
-  select(drug, dosage, PLA, ULU, RIM, OCS, ICS, SLU, SLE)
+  select(drug, dosage, PLA, ULU, RIM, OCS, ICS, SLU, SLE) 
   
 head(bio_sample_test)
 
+
+#single variable plot 
 test_plot <- bio_sample_test %>% 
   ggplot(aes(dosage, PLA, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
   scale_x_continuous(breaks=c(50,100))+
   ggtitle('geom_beeswarm') + labs(x='Dose')+
   theme_few()
+
+
+#gather for small multiples bio markers
+
+bio_sample_forSM <- bio_sample_test %>% 
+  gather(key = variable, value = value, -drug, -dosage)
+
+
+#plot small multiples bio markers 
+bio_sample_forSM %>% 
+  ggplot(aes(x = dosage, y = value, color = drug))+
+  geom_beeswarm(alpha = 0.5, size = 1.5)+
+  scale_x_continuous(breaks = c(50,100))+
+  labs(x = 'Dose', y = 'Value')+
+  theme_few()+
+  facet_wrap(~ variable, ncol = 4)
+
+
+
+
+
 
 
 
