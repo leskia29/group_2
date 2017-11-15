496b693b735a195fcb6c868cdf50be7dc999a290
Marialexia Alfano <marialexia.alfano@colostate.edu>
2017-11-15 12:01:57 -0700

    fixing the labels to invitro/invivo

--- sample_data/ggbeeswarm_sample_data.R
+++ sample_data/ggbeeswarm_sample_data.R
@@ -16,14 +16,14 @@ summary(test_data)
 head(test_data)
 
 
-#select for bio marker variables
+#select for in-vivo marker variables
 bio_sample_test <- test_data %>%
   select(drug, dosage, PLA, ULU, RIM, OCS, ICS, SLU, SLE) 
   
 head(bio_sample_test)
 
 
-#single variable plot for bio marker
+#single variable plot for in-vivo marker
 test_plot <- bio_sample_test %>% 
   ggplot(aes(dosage, PLA, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
@@ -33,20 +33,21 @@ test_plot <- bio_sample_test %>%
 
 test_plot
 
-#gather for small multiples bio markers
+#gather for small multiples in-vivo markers
 bio_sample_forSM <- bio_sample_test %>% 
   gather(key = variable, value = value, -drug, -dosage)
 
 head(bio_sample_forSM)
 
 
-#plot small multiples bio markers 
+#plot small multiples in-vivo markers 
 bio_sample_SMplot <- bio_sample_forSM %>% 
   ggplot(aes(x = dosage, y = value, color = drug))+
   geom_beeswarm(alpha = 0.5, size = 1.5)+
   scale_x_continuous(breaks = c(50,100))+
+  coord_trans(y = "log10")+
   labs(x = 'Dose', y = 'Value')+
-  ggtitle('Biological Measurements of TB Drugs')+
+  ggtitle('In-Vivo Distribution of TB Drugs')+
   theme_few()+
   facet_wrap(~ variable, ncol = 4)
 
@@ -54,14 +55,14 @@ bio_sample_SMplot
 
 #####################################################################
 
-#select for chem marker variables
+#select for in-vitro marker variables
 chem_sample_test <- test_data %>%
   select(drug, dosage, cLogP, huPPB, muPPB, 
          MIC_Erdman, MICserumErd, MIC_Rv, Caseum_binding, MacUptake) 
 
 head(chem_sample_test)
 
-#single variable plot for chem marker
+#single variable plot for in-vitro marker
 test_plot_2 <- chem_sample_test %>% 
   ggplot(aes(dosage, cLogP, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
@@ -71,19 +72,19 @@ test_plot_2 <- chem_sample_test %>%
 
 test_plot_2
 
-#gather for small multiples chem markers
+#gather for small multiples in-vitro markers
 chem_sample_forSM <- chem_sample_test %>% 
   gather(key = variable, value = value, -drug, -dosage)
 
 head(chem_sample_forSM)
 
-#plot small multiples chem markers 
+#plot small multiples in-vitro markers 
 chem_sample_SMplot <- chem_sample_forSM %>% 
   ggplot(aes(x = dosage, y = value, color = drug))+
   geom_beeswarm(alpha = 0.5, size = 1.5)+
   scale_x_continuous(breaks = c(50,100))+
   labs(x = 'Dose', y = 'Value')+
-  ggtitle('Chemical Measurements of TB Drugs')+
+  ggtitle('In-Vitro Distribution of TB Drugs')+
   theme_few()+
   facet_wrap(~ variable, ncol = 4, scale="free")
 
