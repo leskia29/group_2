0746e4a747155fd9a387a761ddbc3e77a5291901
Mackenzie Fry <mfry@colostate.edu>
2017-11-22 13:02:44 -0700

    actual changes with color included

--- graph_examples/example_ggraph_measurements.R
+++ graph_examples/example_ggraph_measurements.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggraph)
 library(dendextend)
 library(factoextra)
-install.packages("factoextra")
+
 
 test_data <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
                              "master/CSV_Files/efficacy_summary.csv"))
@@ -18,13 +18,18 @@ test_data_dend <- test_data %>%
   as.dendrogram(horiz = TRUE, hang = .3)
 
 
-plot(test_data_dend, k = 4,
-     k_colors = "jco",
-     xlab = "",
-     ylab = "",
-     main = "Cluster by Measurements",
-     horiz = TRUE,
-     axes = FALSE)
-par(cex = .6, mar=c(9, 11, 7, 7))
-par(cex = 0.6)
 
+fviz_dend(test_data_dend, k = 6, # Cut in four groups
+          cex = 0.5,
+          xlab = "",
+          ylab = "",
+          main = "Cluster by Measurements",
+          horiz = TRUE,
+          axes = FALSE,
+          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07", "#F09428",
+                       "#18E154"),
+          color_labels_by_k = TRUE, # color labels by groups
+          rect = TRUE, # Add rectangle around groups
+          rect_border = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07", "#F09428",
+                          "#18E154"),
+          rect_fill = TRUE)
