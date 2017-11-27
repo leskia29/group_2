7c4819e12ae7d4512ce32df5fbb8fd61b41bb878
Marialexia Alfano <marialexia.alfano@colostate.edu>
2017-11-27 16:49:57 -0700

    changed labels and names of data sets, fixed log scale for in vivo data, fixed x axis distribution of dosage

--- sample_data/ggbeeswarm_sample_data.R
+++ sample_data/ggbeeswarm_sample_data.R
@@ -17,76 +17,84 @@ head(test_data)
 
 
 #select for in-vivo marker variables
-bio_sample_test <- test_data %>%
-  select(drug, dosage, PLA, ULU, RIM, OCS, ICS, SLU, SLE) 
+vivo_sample_test <- test_data %>%
+  select(drug, dosage, PLA, ULU, RIM, OCS, ICS, SLU, SLE) %>% 
+  mutate(dosage = factor(dosage))
   
-head(bio_sample_test)
+head(vivo_sample_test)
 
 
 #single variable plot for in-vivo marker
-test_plot <- bio_sample_test %>% 
+test_plot <- vivo_sample_test %>% 
   ggplot(aes(dosage, PLA, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
-  scale_x_continuous(breaks=c(50,100))+
   ggtitle('Variable = PLA') + labs(x='Dose')+
   theme_few()
 
 test_plot
 
 #gather for small multiples in-vivo markers
-bio_sample_forSM <- bio_sample_test %>% 
+vivo_sample_forSM <- vivo_sample_test %>% 
   gather(key = variable, value = value, -drug, -dosage)
 
-head(bio_sample_forSM)
+head(vivo_sample_forSM)
 
 
 #plot small multiples in-vivo markers 
-bio_sample_SMplot <- bio_sample_forSM %>% 
+vivo_sample_SMplot <- vivo_sample_forSM %>% 
   ggplot(aes(x = dosage, y = value, color = drug))+
   geom_beeswarm(alpha = 0.5, size = 1.5)+
-  scale_x_continuous(breaks = c(50,100))+
-  coord_trans(y = "log10")+
+  scale_y_log10()+
   labs(x = 'Dose', y = 'Value')+
   ggtitle('In-Vivo Distribution of TB Drugs')+
   theme_few()+
   facet_wrap(~ variable, ncol = 4)
 
-bio_sample_SMplot
+vivo_sample_SMplot
+
+function()
 
 #####################################################################
 
 #select for in-vitro marker variables
-chem_sample_test <- test_data %>%
+vitro_sample_test <- test_data %>%
   select(drug, dosage, cLogP, huPPB, muPPB, 
-         MIC_Erdman, MICserumErd, MIC_Rv, Caseum_binding, MacUptake) 
+         MIC_Erdman, MICserumErd, MIC_Rv, Caseum_binding, MacUptake) %>% 
+  mutate(dosage = factor(dosage))
 
-head(chem_sample_test)
+head(vitro_sample_test)
 
 #single variable plot for in-vitro marker
-test_plot_2 <- chem_sample_test %>% 
+test_plot_2 <- vitro_sample_test %>% 
   ggplot(aes(dosage, cLogP, color = drug))+
   geom_beeswarm(alpha=.5,size=1.5)+
-  scale_x_continuous(breaks=c(50,100))+
   ggtitle('Variable = cLogP') + labs(x='Dose')+
   theme_few()
 
 test_plot_2
 
 #gather for small multiples in-vitro markers
-chem_sample_forSM <- chem_sample_test %>% 
-  gather(key = variable, value = value, -drug, -dosage)
+vitro_sample_forSM <- vitro_sample_test %>% 
+  gather(key = variable, value = value, -drug, -dosage) %>% 
+  mutate(variable = factor(variable, levels = c("Caseum_binding", "cLogP", "huPPB", "muPPB", "MIC_Erdman", "MICserumErd", "MIC_Rv", "MacUptake"),
+                           labels = c("Ex-Vivo \nCaseum \nBinding", "In-Vitro \nLipophilic \nBinding", 
+                                      "Human \nPlasma \nBinding", "Mouse \nPlasma \nBinding", 
+                                      "MIC Erdman \nStrain", "MIC Erdman \nStrain \nwith Serum", "MIC Rv Strain",
+                                      "Macrophage \nUptake (Ratio)"))) 
 
-head(chem_sample_forSM)
+head(vitro_sample_forSM) 
 
 #plot small multiples in-vitro markers 
-chem_sample_SMplot <- chem_sample_forSM %>% 
+vitro_sample_SMplot <- vitro_sample_forSM %>% 
   ggplot(aes(x = dosage, y = value, color = drug))+
   geom_beeswarm(alpha = 0.5, size = 1.5)+
-  scale_x_continuous(breaks = c(50,100))+
   labs(x = 'Dose', y = 'Value')+
   ggtitle('In-Vitro Distribution of TB Drugs')+
   theme_few()+
   facet_wrap(~ variable, ncol = 4, scale="free")
 
-chem_sample_SMplot
+vitro_sample_SMplot
+
+#create function that allows to select DRUG and VARIBALE
+
 
