7cdffff0822b7b5e86d08e915333bedfd59fd9b1
Marialexia Alfano <marialexia.alfano@colostate.edu>
2017-12-03 13:53:59 -0700

    created code for function, works but still need to figure out why it won't select for drug and variable...

---
+++ R/beeswarm_invitro_function.R
@@ -0,0 +1,46 @@
+#in vitro function 
+
+invitro_function <- function(efficacy_summary, variables = c(), drugs = c()){
+  
+  efficacy_summary <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                                      "master/CSV_Files/efficacy_summary.csv"))
+  in_vitro <- efficacy_summary %>%
+    select(drug, dosage, dose_int, cLogP, huPPB, muPPB, 
+           MIC_Erdman, MICserumErd, MIC_Rv, Caseum_binding, MacUptake) %>%
+    rename(Drugs = "drug") %>% 
+    mutate(dosage = factor(dosage)) %>% 
+    unite(dosage_interval, dosage:dose_int, sep = "")
+  
+  in_vitro_SM <- in_vitro %>% 
+    gather(key = variable, value = value, -Drugs, -dosage_interval) %>% 
+    mutate(variable = factor(variable, levels = c("Caseum_binding", "cLogP", "huPPB", "muPPB", "MIC_Erdman", "MICserumErd", "MIC_Rv", "MacUptake"),
+                             labels = c("Caseum \nBinding", "cLogP", 
+                                        "Human \nPlasma \nBinding", "Mouse \nPlasma \nBinding", 
+                                        "MIC Erdman \nStrain", "MIC Erdman \nStrain \nwith Serum", "MIC Rv Strain",
+                                        "Macrophage \nUptake (Ratio)"))) %>% 
+    mutate(dosage_interval = factor(dosage_interval, levels = c("50BID", "100QD")))
+  
+  invitro_variable_filtered <- in_vitro_SM %>% 
+    dplyr::filter(variable %in% variables)
+  
+  invitro_drug_filtered <- in_vitro_SM %>% 
+    dplyr::filter(Drugs %in% drugs)
+  
+  
+  in_vitro_SMplot <- in_vitro_SM %>% 
+    ggplot(aes(x = dosage_interval, y = value, color = Drugs))+
+    geom_beeswarm(alpha = 0.5, size = 1.5)+
+    labs(x = 'Dosage-Interval', y = 'Value')+
+    ggtitle('In-Vitro Distribution of TB Drugs')+
+    theme_few()+
+    facet_wrap(~ variable, ncol = 4, scale="free")
+  
+  return(in_vitro_SMplot)
+  
+}
+
+invitro_function()
+
+#figure out why it's not selecting 
+#should I source the csv file in the function? 
+
---
+++ R/beeswarm_invivo_function.R
@@ -0,0 +1,46 @@
+#invivo function
+
+invivo_function <- function(efficacy_summary, variables = c(), drugs = c()) {
+  
+  efficacy_summary <- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
+                                      "master/CSV_Files/efficacy_summary.csv"))
+  in_vivo <- efficacy_summary %>%
+    select(drug, dosage, dose_int, PLA, ULU, RIM, OCS, ICS, SLU, SLE) %>% 
+    rename(Drugs = "drug") %>% 
+    mutate(dosage = factor(dosage)) %>% 
+    unite(dosage_interval, dosage:dose_int, sep = "")
+  
+  in_vivo_SM <- in_vivo %>% 
+    gather(key = variable, value = value, -Drugs, -dosage_interval) %>% 
+    mutate(variable = factor(variable, levels = c("RIM", "OCS","ICS","ULU","SLU","SLE","PLA"),
+                             labels = c("Rim (of lesion)","Outer Caseum","Inner Caseum","Uninvolved Lung",
+                                        "Standard Lung", "Standard Lesion", "Plasma"))) %>% 
+    mutate(dosage_interval = factor(dosage_interval, levels = c("50BID","100QD")))
+  
+  invivo_variable_filtered <- in_vivo_SM %>% 
+    dplyr::filter(variable %in% variables)
+  
+  invivo_drug_filtered <- in_vivo_SM %>% 
+    dplyr::filter(Drugs %in% drugs)
+  
+  in_vivo_SMplot <- in_vivo_SM %>% 
+    ggplot(aes(x = dosage_interval, y = value, color = Drugs))+
+    geom_beeswarm(alpha = 0.5, size = 1.5)+
+    scale_y_log10()+
+    labs(x = 'Dose', y = 'Value')+
+    ggtitle('In-Vivo Distribution of TB Drugs')+
+    theme_few()+
+    facet_wrap(~ variable, ncol = 4)
+  
+  return(in_vivo_SMplot)
+  
+}
+
+invivo_function(efficacy_summary, variables = c("Rim (of lesion)"), drugs = ("DRUG1"))
+
+invivo_function()
+
+#figure out why it's not selecting 
+#should I source the csv in the function...? 
+
+
