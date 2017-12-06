71856cc368c04242289d5ee0961775f2cc32e3c2
Colleen Brents <cbrents1@rams.colostate.edu>
2017-12-06 11:13:22 -0700

    first part of function works, but adjust margins fixing graphing part for second function

--- R/ggraph_functions.R
+++ R/ggraph_functions.R
@@ -2,11 +2,12 @@
 library(readr)
 library(dplyr)
 library(ggraph)
-library(dendextend)
 #library(factoextra)
 library(ggthemes)
 library(tibble)
 library(dendextend)
+library(ggplot2)
+library(ggdendro)
 
 ##############################by measurement###############################################
 #superfxn <- function(category, variables =c("measurement"))
@@ -14,15 +15,17 @@ library(dendextend)
 input_data<- read_csv(paste0("https://raw.githubusercontent.com/KatieKey/input_output_shiny_group/",
                              "master/CSV_Files/efficacy_summary.csv")) 
 #superfux(df= input_data, category = "by_test" OR "drug")
+df = input_data
+category= "by_test"
 superfxn <- function(df, category) {
-  test_type <- ifelse(colnames(input_data) %in% 
-                         c("cLogP", "huPPB","muPPB", "MIC_Erdman", 
-                           "MICserumErd", "MIC_Rv","Caseum_binding", "MacUptake"), 1, 2) 
+  #test_type <- ifelse(colnames(input_data) %in% 
+                         #c("cLogP", "huPPB","muPPB", "MIC_Erdman", 
+                           #"MICserumErd", "MIC_Rv","Caseum_binding", "MacUptake"), 1, 2) 
                   #makes vecotr #could assign color  1 is invivo  
    #how do we change the inputted data file? (raw data)
    #1.5 choose what you want to do, then clean the data accordngly
    if(category == "by_test"){
-         by_test <- input_data %>% 
+         by_test <- df %>% 
             select(PLA:SLE,cLogP:MacUptake) %>% 
            mutate_all(funs(scale(.))) %>%
            rename(plasma = PLA,
@@ -46,8 +49,8 @@ superfxn <- function(df, category) {
              label <- attr(by_test, "label") 
              ## set label color to red for A and B, to blue otherwise
              attr(by_test, "nodePar") <- list(lab.col=ifelse(label %in% 
-                c("cLogP", "huPPB","muPPB", "MIC_Erdman", "MICserumErd", 
-                  "MIC_Rv","Caseum_binding", "MacUptake"), "red", "blue"))
+                c("macrophage_uptake","cLogP", "huPPB","muPPB", "MIC_Erdman", "MICserumErd", 
+                  "MIC_Rv","Caseum_binding"), "red", "blue"))  # red is invitro
            }
              return(by_test)
          }
@@ -55,28 +58,28 @@ superfxn <- function(df, category) {
          plot_horiz.dendrogram(d, side = TRUE, main = "comparison by test")
          #plot(d, horiz = TRUE, main = "by test", sub="color coded by test type", xlab = "")
          cols <- c("red","blue")
-         legend("topright", legend = c("invivo","invitro"),
+         legend("topright", legend = c("invitro","invivo"),
                 fill = cols, border = cols, bty = "n")
-        # ggdendrogram(d, 
-        #                  segments = TRUE,
-        #                  rotate = TRUE, 
-        #                  labels = TRUE,
-        #                  leaf_labels = TRUE,
-        #                  size = 2,
-        #                  theme_dendro = TRUE)  +
-        #       labs(title = "Comparison by test")
-      } else {
-  by_drug <- input_data %>% 
+         #base plot oldpar<- par(mar xxxx, oma xxx)  run at start
+         # par(oldpar) rest    at begining and end of function ; side effect of function
+         # try ggplot or ggdend with colors 
+             } else {
+  by_drug <- df %>% 
     tidyr::unite(drugdetail, drug:level, sep = "_") %>% #combine identifying data into one column, 
-    mutate_each_(funs(scale(.) %>% as.vector),
-                 vars = c("PLA", "ULU", "RIM", "OCS", "ICS", "SLU", "SLE", "cLogP", "huPPB","muPPB",
+    mutate_at(funs(scale(.) %>% as.vector),
+                 .vars = c("PLA", "ULU", "RIM", "OCS", "ICS", "SLU", "SLE", "cLogP", "huPPB","muPPB",
                           "MIC_Erdman", "MICserumErd", "MIC_Rv", "Caseum_binding", "MacUptake")) %>% #scales
     select(drugdetail, PLA:MacUptake, -ELU, -ESP) %>%  #remove efficacy 
-    tibble::column_to_rownames (var = "drugdetail") %>%  #make drugdetail leaf name!  
+    #tibble::column_to_rownames (var = "drugdetail") %>%  #make drugdetail leaf name! 
+    column_to_rownames (var = "drugdetail") %>%  #make drugdetail leaf name!  ignore warning
+    
     dist() %>% 
     hclust() %>%  #can change method 
     as.dendrogram(horiz = TRUE, hang = .1) 
-  ggdendrogram(by_drug, 
+  #ggdend
+  #as.ggdend(by_drug)
+  library(ggdendro)
+  ggdend(by_drug, 
                segments = TRUE,
                rotate = TRUE, 
                labels = TRUE,
